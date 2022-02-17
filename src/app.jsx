ea73bd216b20faa7d081cac176f058aa349daae5
rajeshmanjunath <rajesh@thisisalice.com>
2022-02-17 22:16:24 +0530

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,5 @@
+
+import { CounterComponent } from '@raj23manj/components-lib';
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
@@ -7,6 +9,7 @@ const App = () => {
   return(
     <div>
       Hello from App component Robox!!!
+      <CounterComponent />
     </div>
   );
 };
